cf80f2ca58788d5b401eb972f8695dd4e38937e8
Gleb Korobov <gkorobov9@gmail.com>
2023-09-27 17:18:41 +0300

    first commit

---
+++ src/main/java/com/ua/glebkorobov/javapracticaltestassignment/JavaPracticalTestAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.ua.glebkorobov.javapracticaltestassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPracticalTestAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaPracticalTestAssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ua/glebkorobov/javapracticaltestassignment/JavaPracticalTestAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ua.glebkorobov.javapracticaltestassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaPracticalTestAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
